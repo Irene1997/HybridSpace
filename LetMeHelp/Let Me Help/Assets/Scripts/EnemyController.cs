212e3432ddd7cd6510e6a41cbb2f0d822eed483e
John <HylianNanobyte@Gmail.com>
2018-11-20 17:43:25 +0100

    Added basic enemies, including EnemyController and GameController scripts

---
+++ LetMeHelp/Let Me Help/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+
+    Vector3 playerPosition;
+    NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+	}
+
+    public void Initialize() {
+        playerPosition = GameController.instance.playerPosition;
+        agent.SetDestination(playerPosition);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ LetMeHelp/Let Me Help/Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public static GameController instance;
+
+    public GameObject playerObject;
+    public Vector3 playerPosition;
+    public GameObject[] enemies;
+
+    // Use this for initialization
+    void Start() {
+        instance = this;
+        playerPosition = playerObject.transform.position;
+        foreach(GameObject enemy in enemies) {
+            enemy.GetComponent<EnemyController>().Initialize();
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
