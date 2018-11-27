6fd8b9208d40c4f4a051351aa9c9895e2e8fdb10
John <HylianNanobyte@Gmail.com>
2018-11-27 14:45:11 +0100

    Fixed the bug where the enemies are not following the player

--- LetMeHelp/Let Me Help/Assets/Scripts/EnemyController.cs
+++ LetMeHelp/Let Me Help/Assets/Scripts/EnemyController.cs
@@ -4,18 +4,16 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour {
-
-    Vector3 playerPosition;
+    
     NavMeshAgent agent;
 
 	// Use this for initialization
 	void Start () {
         agent = gameObject.GetComponent<NavMeshAgent>();
-        playerPosition = GameController.Instance.player.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        agent.SetDestination(playerPosition);
+        agent.SetDestination(GameController.Instance.player.transform.position);
 	}
 }
