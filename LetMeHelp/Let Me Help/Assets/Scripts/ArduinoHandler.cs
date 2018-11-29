d48c76354888a86d6a6806d24fb8880de6af461b
John <HylianNanobyte@Gmail.com>
2018-11-29 10:12:34 +0100

    Arduino preps

---
+++ LetMeHelp/Let Me Help/Assets/Scripts/ArduinoHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class ArduinoHandler : MonoBehaviour {
+    SerialPort stream;
+
+	// Use this for initialization
+	void Start () {
+        stream = new SerialPort("COM4", 9600);
+        stream.ReadTimeout = 50;
+        stream.Open();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
