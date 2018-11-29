f905dc42e7c7b7a53833ca0a3f012fd5c4b98cc2
John <HylianNanobyte@Gmail.com>
2018-11-29 12:30:11 +0100

    copied some code to make arduino communication work

--- MazeArduino/MazeArduino.ino
+++ MazeArduino/MazeArduino.ino
@@ -1,9 +1,58 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SoftwareSerial.h>
+#include <SerialCommand.h>
 
+SerialCommand sCmd;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT); 
+  
+  Serial.begin(9600);
+  while (!Serial);
+
+  sCmd.addCommand("PING", pingHandler);
+  sCmd.addCommand("ECHO", echoHandler);
+  sCmd.addCommand("LED_ON", led_on);
+  sCmd.addCommand("LED_OFF", led_off);
+  sCmd.addDefaultHandler(errorHandler);
+
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  // Your operations here
 
+  if (Serial.available() > 0)
+    sCmd.readSerial();
+
+  delay(50);  
 }
+
+void led_on(){
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void led_off(){
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void pingHandler ()
+{
+  Serial.println("PONG");
+}
+
+void echoHandler ()
+{
+  char *arg;
+  arg = sCmd.next();
+  if (arg != NULL)
+    Serial.println(arg);
+  else
+    Serial.println("nothing to echo");
+}
+
+void errorHandler (const char *command)
+{
+  Serial.println("Some error happened");
+}
+
