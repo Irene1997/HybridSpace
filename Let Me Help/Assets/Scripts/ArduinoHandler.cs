9db8469e67acc47ca65981aea9d31455ad42a09d
John <HylianNanobyte@Gmail.com>
2018-12-13 13:13:47 +0100

    Unity can now handle multiple Arduinos

--- Let Me Help/Assets/Scripts/ArduinoHandler.cs
+++ Let Me Help/Assets/Scripts/ArduinoHandler.cs
@@ -6,63 +6,93 @@ using System.IO.Ports;
 
 public class ArduinoHandler : MonoBehaviour {
     /* The serial port where the Arduino is connected. */
-    [Tooltip("The serial port where the Arduino is connected")]
-    public string port = "COM3";
+    [Tooltip("The serial ports where the Arduinos are connected")]
+    public string[] ports = { "COM3", "COM4" };
     /* The baudrate of the serial port. */
     [Tooltip("The baudrate of the serial port")]
     public int baudrate = 9600;
 
-    private SerialPort stream;
+    IDictionary<string, Arduino> arduinos;
 
     public void Start() {
-        Open();
-        StartCoroutine(AsynchronousReadFromArduino((string s) => ReadMessage(s), () => Debug.LogError("Error!"), 10000f));
-    }
+        arduinos = new Dictionary<string, Arduino>();
 
-    public void Open() {
-        // Opens the serial port
-        stream = new SerialPort(port, baudrate);
-        stream.ReadTimeout = 50;
-        stream.Open();
-        WriteToArduino("ASK_STATE");
-        //this.stream.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+        foreach (string port in ports) {
+            Arduino arduino = Arduino.StartArduino(port, port);
+            if (arduino != null) {
+                StartCoroutine(arduino.Coroutine((string s) => ReadMessage(port + s)));
+                arduinos.Add(new KeyValuePair<string, Arduino>(port, arduino));
+            }
+        }
 
-        //WriteToArduino("PING");
-        //WriteToArduino("ECHO echo");
-        //WriteToArduino("LED_OFF");
-        //WriteToArduino("ERRORTEST");
+        foreach (Arduino arduino in arduinos.Values) {
+            arduino.Write("LED_ON");
+            arduino.Write("ASK_STATE");
+        }
     }
 
     void ReadMessage(string message) {
-        Debug.Log("Received '" + message + "' from the Arduino.");
+        Debug.Log("Received '" + message + "' from an Arduino.");
         switch (message) {
-            case "Open":
+            case "COM3Open":
                 GameController.Instance.doors[0].Open();
                 break;
-            case "Close":
+            case "COM3Close":
                 GameController.Instance.doors[0].Close();
                 break;
+            case "COM4Open":
+                GameController.Instance.doors[1].Open();
+                break;
+            case "COM4Close":
+                GameController.Instance.doors[1].Close();
+                break;
             default:
+                Debug.Log("Message '" + message + "' could not be processed.");
                 break;
         }
     }
 
-    public void WriteToArduino(string message) {
-        // Send the request
-        stream.WriteLine(message);
-        stream.BaseStream.Flush();
+    public void OnApplicationQuit() {
+        Debug.Log("Closing Arduino connections...");
+        foreach (Arduino arduino in arduinos.Values) {
+            arduino.Close();
+        }
     }
+}
 
-    public string ReadFromArduino(int timeout = 0) {
-        stream.ReadTimeout = timeout;
+class Arduino {
+    public string name, port;
+    public SerialPort stream;
+
+    public static Arduino StartArduino(string name, string port, int baudrate = 9600) {
+        SerialPort stream;
         try {
-            return stream.ReadLine();
-        } catch (TimeoutException) {
+            stream = new SerialPort(port, baudrate) { ReadTimeout = 50 };
+            stream.Open();
+            return new Arduino(name, port, stream);
+        } catch (Exception e) {
+            Debug.LogWarning(e);
             return null;
         }
     }
 
-    public IEnumerator AsynchronousReadFromArduino(Action<string> callback, Action fail = null, float timeout = float.PositiveInfinity) {
+    Arduino(string name, string port, SerialPort stream) {
+        this.name = name;
+        this.port = port;
+        this.stream = stream;
+    }
+
+    public void Write(string message) {
+        stream.WriteLine(message);
+        stream.BaseStream.Flush();
+    }
+
+    public IEnumerator Coroutine(Action<string> callback) {
+        return AsynchronousReadFromArduino(stream, callback, () => Debug.LogError("Error!"), 10000f);
+    }
+
+
+    IEnumerator AsynchronousReadFromArduino(SerialPort stream, Action<string> callback, Action fail = null, float timeout = float.PositiveInfinity) {
         DateTime initialTime = DateTime.Now;
         DateTime nowTime;
         TimeSpan diff = default(TimeSpan);
@@ -93,8 +123,7 @@ public class ArduinoHandler : MonoBehaviour {
         yield return null;
     }
 
-    public void OnApplicationQuit() {
-        Debug.Log("Closing Arduino connection...");
+    public void Close() {
         stream.Close();
     }
 }
